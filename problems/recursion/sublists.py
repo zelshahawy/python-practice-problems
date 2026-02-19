9cdb31e1c61e5ad54dba22b13817d007eda92eba
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2026-02-18 21:13:54 -0600

    finished sum cubes and sublists

--- problems/recursion/sublists.py
+++ problems/recursion/sublists.py
@@ -1,13 +1,30 @@
-def sublists(lst):
+from typing import Any
+
+
+def sublists(lst: list[Any]) -> list[list[Any]]:
     """
     Computes all sublists of the input list.
     Input:
         lst: list of values
-    
+
     Returns: (list of list of values) list of all sublists of lst.
     """
+    if not lst:
+        return [[]]
+    result: list[list[Any]] = []
 
-    pass
+    def backtracking(sublist: list[Any], index: int):
+        if index == len(lst):
+            result.append(sublist.copy())
+            return
+
+        backtracking(sublist, index + 1)
+        backtracking(sublist + [lst[index]], index + 1)
+
+        return
+
+    backtracking([], 0)
+    return result
 
 
 #############################################################
@@ -18,15 +35,18 @@ def sublists(lst):
 #############################################################
 
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 import test_utils as utils
 
+
 def do_test_sublists(lst):
     original_lst = list(lst)
-    recreate_msg = utils.gen_recreate_msg('sublists', original_lst)
-    expected = [[x for j, x in enumerate(lst) if i ^ (2 ** j) < i] 
-        for i in range(2 ** len(lst))]
+    recreate_msg = utils.gen_recreate_msg("sublists", original_lst)
+    expected = [
+        [x for j, x in enumerate(lst) if i ^ (2**j) < i] for i in range(2 ** len(lst))
+    ]
     actual = sublists(lst)
     utils.check_none(actual, recreate_msg)
     utils.check_type(actual, expected, recreate_msg)
@@ -37,61 +57,80 @@ def do_test_sublists(lst):
 
 
 def test_sublists_1():
-    do_test_sublists(['apple'])
+    do_test_sublists(["apple"])
+
 
 def test_sublists_2():
     do_test_sublists([12])
 
+
 def test_sublists_3():
     do_test_sublists([True])
 
+
 def test_sublists_4():
-    do_test_sublists(['A', 'B'])
+    do_test_sublists(["A", "B"])
+
 
 def test_sublists_5():
     do_test_sublists(["apple", "tomato"])
 
+
 def test_sublists_6():
-    do_test_sublists(['A', 'B', 'C'])
+    do_test_sublists(["A", "B", "C"])
+
 
 def test_sublists_7():
     do_test_sublists([50, 150, 100])
 
+
 def test_sublists_8():
-    do_test_sublists(['A', 'B', 'C', 'D'])
+    do_test_sublists(["A", "B", "C", "D"])
+
 
 def test_sublists_9():
     do_test_sublists([50, 0, -1, 10])
 
+
 def test_sublists_10():
     do_test_sublists([50, 0, -1, 10, 5])
 
+
 def test_sublists_11():
-    do_test_sublists(['A', 'B', 'C', 'D', 'E'])
+    do_test_sublists(["A", "B", "C", "D", "E"])
+
 
 def test_sublists_12():
-    do_test_sublists(['U', 'V', 'W', 'X', 'Y', 'Z'])
+    do_test_sublists(["U", "V", "W", "X", "Y", "Z"])
+
 
 def test_sublists_13():
-    do_test_sublists(['water', 'apple', 'tomato', 'zucchini', 'corn', 'stew'])
+    do_test_sublists(["water", "apple", "tomato", "zucchini", "corn", "stew"])
+
 
 def test_sublists_14():
     do_test_sublists(list(range(0, 70, 10)))
 
+
 def test_sublists_15():
     do_test_sublists(list(range(70, 0, -10)))
 
+
 def test_sublists_16():
-    do_test_sublists(list('estuary'))
+    do_test_sublists(list("estuary"))
+
 
 def test_sublists_17():
     do_test_sublists([1, 2, 3, 5, 8, 13, 21])
 
+
 def test_sublists_18():
     do_test_sublists([1, -2, 3, -5, 8, -13, 21])
 
+
 def test_sublists_19():
     do_test_sublists([8, 6, 7, 5, 3, 0, 9])
+
 
 def test_sublists_20():
     do_test_sublists([1, 2, 3, 5, 8, 13, 21, 34])
