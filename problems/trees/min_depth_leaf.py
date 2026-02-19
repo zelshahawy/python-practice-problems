2949f4b67eabe5ba0d8701833d43a27309e35e0d
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2026-02-18 22:07:21 -0600

    implemented some stuff

--- problems/trees/min_depth_leaf.py
+++ problems/trees/min_depth_leaf.py
@@ -1,133 +1,122 @@
 from tree import Tree
 
 
-
-def min_depth_leaf(tree):
+def min_depth_leaf(tree: Tree):
     """
     Computes the minimum depth of a leaf in the tree (length of shortest
     path from the root to a leaf).
     Input:
         tree: a Tree instance.
-    
+
     Returns: (integer) the minimum depth of of a leaf in the tree.
     """
     if not tree.children:
         return 0
-    return min(min_depth_leaf(child) for child in tree.children) + 1
-    
-def min_depth_leaf_custom(tree, custom_depth=0):
-    """
-    Finds the custom minimum depth of a leaf in the tree. By default,
-    it finds the absolute minimum depth.
-    
-    Inputs:
-    tree (Tree): The tree to find the minimum depth in.
-    Custom_depth (int, optional): The custom depth to start from. Defaults to 0.
-    
-    Returns (int): The custom minimum depth of a leaf in the tree.
-    """
-    def lst_of_depth(tree, depth=0):
-        """
-        Recursively finds the depths of all leaves in the tree and returns
-        them in a sorted list.
-        
-        Parameters:
-        tree (Tree): The tree to find the depths in.
-        depth (int, optional): The current depth. Defaults to 0.
-        
-        Returns:
-        list: A sorted list of the depths of all leaves in the tree.
-        """
-        if not tree.children:
-            return [depth]
-        return sorted(new_depth for child in tree.children for new_depth \
-            in lst_of_depth(child, depth + 1))
-    tree_lst_of_depth = lst_of_depth(tree)
-    if len(tree_lst_of_depth) > custom_depth:
-        return tree_lst_of_depth[custom_depth]
-    else:
-        return tree_lst_of_depth[-1]
+    tree_heights = [min_depth_leaf(kid) for kid in tree.children]
+    return min(tree_heights) + 1
 
 
 import sys
 import pytest
 import util
 
-sys.path.append('../')
+sys.path.append("../")
 
 import test_utils as utils
 import tree_test_utils as tree_utils
 
+
 def do_test_min_depth_leaf(trees_and_original_trees, tree_name, expected):
     trees, original_trees = trees_and_original_trees
-    recreate_msg = tree_utils.gen_recreate_msg_with_trees('min_depth_leaf', tree_name)
+    recreate_msg = tree_utils.gen_recreate_msg_with_trees("min_depth_leaf", tree_name)
     actual = min_depth_leaf(trees[tree_name])
     utils.check_none(actual, recreate_msg)
     utils.check_type(actual, expected, recreate_msg)
     utils.check_equals(actual, expected, recreate_msg)
-    tree_utils.check_tree_unmodified(trees[tree_name], original_trees[tree_name], 
-                                recreate_msg)
+    tree_utils.check_tree_unmodified(
+        trees[tree_name], original_trees[tree_name], recreate_msg
+    )
 
 
 def test_min_depth_leaf_1(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_1", 1)
 
+
 def test_min_depth_leaf_2(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_2", 1)
+
 
 def test_min_depth_leaf_3(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_3", 5)
 
+
 def test_min_depth_leaf_4(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_4", 4)
+
 
 def test_min_depth_leaf_5(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_5", 3)
 
+
 def test_min_depth_leaf_6(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_6", 2)
+
 
 def test_min_depth_leaf_7(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_7", 2)
 
+
 def test_min_depth_leaf_8(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_8", 2)
+
 
 def test_min_depth_leaf_9(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_9", 49)
 
+
 def test_min_depth_leaf_10(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_10", 2)
+
 
 def test_min_depth_leaf_11(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_11", 6)
 
+
 def test_min_depth_leaf_12(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_12", 2)
+
 
 def test_min_depth_leaf_13(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_13", 4)
 
+
 def test_min_depth_leaf_14(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_14", 4)
+
 
 def test_min_depth_leaf_15(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_15", 3)
 
+
 def test_min_depth_leaf_16(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_16", 4)
+
 
 def test_min_depth_leaf_17(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_17", 2)
 
+
 def test_min_depth_leaf_18(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_18", 6)
+
 
 def test_min_depth_leaf_19(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_19", 3)
 
+
 def test_min_depth_leaf_20(trees_min_depth_leaf):
     do_test_min_depth_leaf(trees_min_depth_leaf, "tree_20", 2)
+
 
 @pytest.fixture(scope="session")
 def trees_min_depth_leaf():
@@ -135,6 +124,7 @@ def trees_min_depth_leaf():
     Fixture for loading the trees for min_depth_leaf
     """
     return get_trees()
+
 
 def get_trees():
     trees = util.load_trees("sample_trees.json")
--- problems/trees/prune_tree.py
+++ problems/trees/prune_tree.py
@@ -1,7 +1,8 @@
 from tree import Tree
 
-def prune_tree(tree, keys_to_discard):
-    '''
+
+def prune_tree(tree: Tree, keys_to_discard):
+    """
     Returns a new tree with that is identical to the original tree, except
     that any node whose key is in keys_to_discard is removed, along with its
     descendants. If the key of the root is in keys_to_discard, then
@@ -10,106 +11,134 @@ def prune_tree(tree, keys_to_discard):
     Inputs:
         tree: a Tree instance.
         keys_to_discard: set of keys.
-    
+
     Returns: (Tree) the pruned tree.
-    '''
-    if tree.key in keys_to_discard:
+    """
+    if not tree or tree.key in keys_to_discard:
         return None
-    if not tree.children:
-        return tree
     new_tree = Tree(tree.key, tree.value)
     for child in tree.children:
-        new_child = prune_tree(child, keys_to_discard=keys_to_discard)
-        if new_child is not None:
-            new_tree.add_child(new_child)
+        pruned_child = prune_tree(child, keys_to_discard)
+        if pruned_child:
+            new_tree.children.append(pruned_child)
     return new_tree
 
 
 import sys
 import pytest
 import util
-sys.path.append('../')
+
+sys.path.append("../")
 
 import test_utils as utils
 import tree_test_utils as tree_utils
 
+
 def do_test_prune_tree(trees_original_expected, tree_name, keys_to_prune):
     trees, original_trees, expected_trees = trees_original_expected
-    recreate_msg = tree_utils.gen_recreate_msg_with_trees('prune_tree', 
-                                                    tree_name, keys_to_prune)
+    recreate_msg = tree_utils.gen_recreate_msg_with_trees(
+        "prune_tree", tree_name, keys_to_prune
+    )
     actual = prune_tree(trees[tree_name], keys_to_prune)
     utils.check_none(actual, recreate_msg)
     utils.check_type(actual, expected_trees[tree_name], recreate_msg)
     tree_utils.check_tree_equals(actual, expected_trees[tree_name], recreate_msg)
-    tree_utils.check_tree_unmodified(trees[tree_name], original_trees[tree_name], 
-                                     recreate_msg)
+    tree_utils.check_tree_unmodified(
+        trees[tree_name], original_trees[tree_name], recreate_msg
+    )
 
 
 def test_prune_tree_1(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_1", {'D', 'B'})
+    do_test_prune_tree(trees_prune_tree, "tree_1", {"D", "B"})
+
 
 def test_prune_tree_2(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_2", {'C', 'E'})
+    do_test_prune_tree(trees_prune_tree, "tree_2", {"C", "E"})
+
 
 def test_prune_tree_3(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_3", {'E'})
+    do_test_prune_tree(trees_prune_tree, "tree_3", {"E"})
+
 
 def test_prune_tree_4(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_4", {'b', 'I', 'J', 'c', 'M'})
+    do_test_prune_tree(trees_prune_tree, "tree_4", {"b", "I", "J", "c", "M"})
+
 
 def test_prune_tree_5(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_5", {'Q', 'G', 'C'})
+    do_test_prune_tree(trees_prune_tree, "tree_5", {"Q", "G", "C"})
+
 
 def test_prune_tree_6(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_6", {'V2'})
+    do_test_prune_tree(trees_prune_tree, "tree_6", {"V2"})
+
 
 def test_prune_tree_7(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_7", {'V9'})
+    do_test_prune_tree(trees_prune_tree, "tree_7", {"V9"})
+
 
 def test_prune_tree_8(trees_prune_tree):
     do_test_prune_tree(trees_prune_tree, "tree_8", set())
 
+
 def test_prune_tree_9(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_9", {'V9', 'V8'})
+    do_test_prune_tree(trees_prune_tree, "tree_9", {"V9", "V8"})
+
 
 def test_prune_tree_10(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_10", 
-        {'V11', 'V8', 'V7', 'V4', 'V5'})
+    do_test_prune_tree(trees_prune_tree, "tree_10", {"V11", "V8", "V7", "V4", "V5"})
+
 
 def test_prune_tree_11(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_11", {'V80', 'V90'})
+    do_test_prune_tree(trees_prune_tree, "tree_11", {"V80", "V90"})
+
 
 def test_prune_tree_12(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_12", 
-        {'V26', 'V9', 'V1', 'V6', 'V27'})
+    do_test_prune_tree(trees_prune_tree, "tree_12", {"V26", "V9", "V1", "V6", "V27"})
+
 
 def test_prune_tree_13(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_13", {'V0', 'V1', 'V4', 'V3'})
+    do_test_prune_tree(trees_prune_tree, "tree_13", {"V0", "V1", "V4", "V3"})
+
 
 def test_prune_tree_14(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_14", 
-        {'V2', 'V0', 'V1', 'V7', 'V4', 'V6', 'V3', 'V5'})
+    do_test_prune_tree(
+        trees_prune_tree, "tree_14", {"V2", "V0", "V1", "V7", "V4", "V6", "V3", "V5"}
+    )
+
 
 def test_prune_tree_15(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_15", 
-        {'V2', 'V48', 'V49', 'V46', 'V45', 'V47'})
+    do_test_prune_tree(
+        trees_prune_tree, "tree_15", {"V2", "V48", "V49", "V46", "V45", "V47"}
+    )
+
 
 def test_prune_tree_16(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_16", {'V10', 'V20', 'V30'})
+    do_test_prune_tree(trees_prune_tree, "tree_16", {"V10", "V20", "V30"})
+
 
 def test_prune_tree_17(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_17", {'V3', 'V7'})
+    do_test_prune_tree(trees_prune_tree, "tree_17", {"V3", "V7"})
+
 
 def test_prune_tree_18(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_18", 
-        {'V26', 'V27', 'V24', 'V21', 'V28', 'V25', 'V23', 'V22'})
+    do_test_prune_tree(
+        trees_prune_tree,
+        "tree_18",
+        {"V26", "V27", "V24", "V21", "V28", "V25", "V23", "V22"},
+    )
+
 
 def test_prune_tree_19(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_19", {'V18'})
+    do_test_prune_tree(trees_prune_tree, "tree_19", {"V18"})
+
 
 def test_prune_tree_20(trees_prune_tree):
-    do_test_prune_tree(trees_prune_tree, "tree_20", 
-        {'V12', 'V42', 'V46', 'V7', 'V30', 'V29', 'V60', 'V17', 'V36'})
+    do_test_prune_tree(
+        trees_prune_tree,
+        "tree_20",
+        {"V12", "V42", "V46", "V7", "V30", "V29", "V60", "V17", "V36"},
+    )
+
 
 @pytest.fixture(scope="session")
 def trees_prune_tree():
@@ -119,6 +148,7 @@ def trees_prune_tree():
     trees, original_trees = get_trees()
     expected_trees = util.load_trees("sample_trees_pruned.json")
     return trees, original_trees, expected_trees
+
 
 def get_trees():
     trees = util.load_trees("sample_trees.json")
